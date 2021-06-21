b23542f3c56ba915c0a90a070110c692dee3add1
maiki dev :) <64388710+cyberteck-max@users.noreply.github.com>
2021-06-21 13:20:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ apt-get install -y nodejs libwebp ffmpeg wget tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
-echo "Sabrina Estável! Digite npm start para iniciar. uma criação de Dev Java"
+echo "selfbot Estável! Digite npm start para iniciar. uma criação de Dev Java"
